b63804980fe1ee7f78250739544f7e5b66ef6b17
mazhengm2 <mazhengm2@hotmail.com>
2024-03-06 22:20:19 +0100

    add utils, where API credentials are saved

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,18 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = ""
+# Put your name
+key_owner = ""
+# openAI model to use
+model_name = "gpt-3.5-turbo-instruct" 
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
